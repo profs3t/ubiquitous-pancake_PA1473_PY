ae539dfeb1dbc17c4eca84d3b15c1b09af5410a4
Liehl <emil.liehl@hotmail.se>
2022-04-19 13:36:15 +0200

    Basic info/def
    Defined some basic information and definitions.

--- project/main.py
+++ project/main.py
@@ -2,6 +2,33 @@
 import sys
 import __init__
 
+from pybricks import robotics
+from pybricks.hubs import EV3Brick
+from pybricks.ev3devices import Motor, ColorSensor, UltrasonicSensor, TouchSensor
+from pybricks.parameters import Port, Stop, Direction, Button, Color
+from pybricks.robotics import DriveBase
+
+# Robot def
+ev3 = EV3Brick()
+
+# Motor def
+left_motor = Motor(Port.C)
+right_motor = Motor(Port.B)
+lift_motor = Motor(Port.A)
+robot = DriveBase(left_motor, right_motor, wheel_diameter = 47, axle_track = 128)
+
+# Sensor def
+light_sensor = ColorSensor(Port.S3)
+ultra_sensor = UltrasonicSensor(Port.S4)
+touch_sensor = TouchSensor(Port.S1)
+
+# Constant Values
+DRIVE_SPEED = 60
+TURN_RADIUS = 10
+robot.settings(DRIVE_SPEED, 100, 50)
+
+
+
 def main(): # Main method
     return 0
 
