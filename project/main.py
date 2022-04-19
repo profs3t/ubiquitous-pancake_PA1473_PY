728c25c4307305726c2e40f17b8d07f8552d2f4f
Liehl <emil.liehl@hotmail.se>
2022-04-19 14:37:06 +0200

    follow path
    Follow predetermined path and follow specified path done for testing.

--- project/main.py
+++ project/main.py
@@ -18,7 +18,7 @@ lift_motor = Motor(Port.A)
 robot = DriveBase(left_motor, right_motor, wheel_diameter = 47, axle_track = 128)
 
 # Sensor def
-light_sensor = ColorSensor(Port.S3)
+color_sensor = ColorSensor(Port.S3)
 ultra_sensor = UltrasonicSensor(Port.S4)
 touch_sensor = TouchSensor(Port.S1)
 
@@ -26,11 +26,50 @@ touch_sensor = TouchSensor(Port.S1)
 DRIVE_SPEED = 60
 TURN_RADIUS = 10
 robot.settings(DRIVE_SPEED, 100, 50)
+PROPORTIONAL_GAIN = 1.2
 
+# Color Values
+WHITE = 85
+BLACK = 9
+#RED = 
+#BLUE = 
+#YELLOW = 
+#BROWN = 
+#PINK = 
+#GREEN = 
+
+find_color_list = [BLACK, WHITE, GREEN]
+
+def path_find(find_color_list): #Follow a predetermened path
+    use_color = color_sensor.reflection()
+    threshold = (use_color + WHITE) / 2
+    while len(find_color_list) > 1:
+        if color_sensor.reflection() == find_color_list[0]:
+            find_color_list.pop(0)
+            path_find(find_color_list)
+        # Calculate the deviation from the threshold.
+        deviation = color_sensor.reflection() - threshold
+
+        # Calculate the turn rate.
+        turn_rate = PROPORTIONAL_GAIN * deviation
+
+        # Set the drive base speed and turn rate.
+        robot.drive(DRIVE_SPEED, turn_rate)
+
+
+def find_item():
+    distance = ultra_sensor.distance(silent=False)
+    
+
+
+def pick_up_item(): #Picks up an item
+    
 
 
 def main(): # Main method
     return 0
 
+
 if __name__ == '__main__':
     sys.exit(main())
+
