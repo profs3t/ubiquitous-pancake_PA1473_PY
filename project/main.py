95a7b33e34a5f3d34dda857efb0207afa3042489
Liehl <emil.liehl@hotmail.se>
2022-04-27 16:02:32 +0200

    Changes after testing
    Changed some things after testing.

--- project/main.py
+++ project/main.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env pybricks-micropython
 import sys
-import __init__
+#import __init__
 import math
 import time
 
@@ -27,35 +27,36 @@ ultra_sensor = UltrasonicSensor(Port.S4)
 touch_sensor = TouchSensor(Port.S1)
 
 # Constant Values
-DRIVE_SPEED = 60
+DRIVE_SPEED = 20
 TURN_RADIUS = 10
 #robot.settings(DRIVE_SPEED, 100, 50)
-PROPORTIONAL_GAIN = 1.2
+PROPORTIONAL_GAIN = 0.8
 
 robotLen = 150 # MÄT!
 
 # Color Values
-WHITE = ()
-BLACK = ()
-BLUE = ()
-MAGENTA = ()
-YELLOW = ()
-BROWN = ()
-PINK = ()
-GREEN = ()
-LIGHTGREEN = ()
+WHITE = (95, 79, 100)
+BLACK = (8, 7, 14)
+BLUE = (14, 26, 70)
+MAGENTA = (17, 12, 60)
+YELLOW = (80, 51, 19)
+BROWN = (18, 8, 17)
+PINK = (73, 31, 54)
+GREEN = (22, 17, 18)
+LIGHTGREEN = (14, 35, 31)
+GREY = (9, 7, 18)
 
-find_color_list = [BLACK, WHITE]
+#find_color_list = [BLACK, WHITE]
 
 def path_find(find_color_list): #Follow a predetermened path
     use_color = colorMix(color_sensor.rgb())
-    threshold = (use_color + WHITE) / 2
+    threshold = (use_color + colorMix(WHITE)) / 2
     while len(find_color_list) > 1:
-        if color_sensor.rgb() == find_color_list[0]:
+        if color_sensor.rgb() == find_color_list[1]:
             find_color_list.pop(0)
             path_find(find_color_list)
         # Calculate the deviation from the threshold.
-        deviation = colorMix(color_sensor.rgb()) - threshold
+        deviation = threshold - colorMix(color_sensor.rgb())
 
         # Calculate the turn rate.
         turn_rate = PROPORTIONAL_GAIN * deviation
@@ -65,56 +66,28 @@ def path_find(find_color_list): #Follow a predetermened path
 
 def colorMix(rgbColor):
     color = 0
-    for i in rgbColor:
-        color += rgbColor[i]
-        #color = rgbColor[0] + rgbColor[1] + rgbColor[2]
+    #for i in rgbColor:
+        #color += rgbColor[i]
+    color = rgbColor[0] + rgbColor[1] + rgbColor[2]
     return color
 
-"""
 def find_item():
-    closestObj = 100000
-    degTurn = 360
-    for _ in range(72):
-        robot.turn(5)
-        distance = ultra_sensor.distance(silent=False)
-        if closestObj > distance:
-            closestObj = distance
+    robot.straight(100)
+    if color_sensor.rgb() == BROWN:
+        turn = True
+        background = BROWN
+    elif color_sensor.rgb() == GREY:
+        turn = False
+        background = GREY
 
-    minDistance = closestObj - 10
-    maxDistance = closestObj + 10
-
-    done = False
-    while done is False:
-        robot.turn(5)
-        degTurn -= 5
-        if minDistance < ultra_sensor.dictance(silent=False) < maxDistance:
-            hypotenuse = ultra_sensor.dictance(silent=False)
-            robot.turn(degTurn)
-            done = True
-
-    if degTurn != 180:
-        radTurn = math.radians(degTurn)
-        closeCathetus = abs(hypotenuse * math.cos(radTurn))
-        farCathetus = abs(hypotenuse * math.sin(radTurn))
-
-        robot.straight(closeCathetus)
-        if degTurn < 180:
-            robot.turn(-90)
-        elif degTurn > 180:
-            robot.turn(90)
-        robot.straight(farCathetus - robotLen)
-    else:
-        robot.drive(hypotenuse - robotLen)
-
-    pick_up_item()
-"""
-
-def find_item_red_or_blue():
     use_color = YELLOW
-    threshold = (use_color + BLACK) / 2
+    threshold = (colorMix(use_color) + colorMix(background)) / 2
     while touch_sensor.pressed() == False:
         # Calculate the deviation from the threshold.
-        deviation = colorMix(color_sensor.rgb()) - threshold
+        if turn == True:
+            deviation = threshold - colorMix(color_sensor.rgb())
+        elif turn == False:
+            deviation = colorMix(color_sensor.rgb()) - threshold
 
         # Calculate the turn rate.
         turn_rate = PROPORTIONAL_GAIN * deviation
@@ -122,51 +95,46 @@ def find_item_red_or_blue():
         # Set the drive base speed and turn rate.
         robot.drive(DRIVE_SPEED, turn_rate)
 
-def check_if_elevated_heavy():
-    lift_motor.run_time(5, 5, Stop.HOLD, True)
-    if lift_motor.angle() < 10:
-        pick_up_elevated()
+    check_if_elevated()
 
-def check_if_elevated_sonic():
-    distance = ultra_sensor.distance(silent=False)
-    if distance >= 200:
-        robot.straight(-50)
-        pick_up_elevated()
-    else:
-        pick_up_item()
+def check_if_elevated():
+    lift_motor.run_time(50, 1000, Stop.HOLD, True)
+    print('fist check', lift_motor.angle())
+    if lift_motor.angle() < 40:
+        lift_motor.run_target(200, -10)
+        robot.straight(-150)
+        pick_up()
 
 def check_pick_up():
     if touch_sensor.pressed() == False:
-        ev3.screen.load_image('knocked_out.png')
+        #ev3.screen.load_image('knocked_out.png')
+        print('droped pallet')
         time.sleep(1)
 
 def pick_up_item(): #Picks up an item
-    #lift_motor.run_time(5, 5, Stop.HOLD, True)
-    lift_motor.run_target(5, 15, Stop.HOLD, True)
+    lift_motor.run_time(50, 1000, Stop.HOLD, True)
+    #lift_motor.run_target(500, 40, Stop.HOLD, True)
 
-    check_pick_up()
+    #check_pick_up()
 
-def pick_up_elevated():
-    # Lift lift til pallet height
-    lift_motor.run_target(5, 20, Stop.HOLD, True)
+def pick_up():
+    print('pick_up')
+    lift_motor.run_target(200, -10)
+    lift_motor.run_target(200, 35, Stop.HOLD, True)
     
-    robot.straight(50) #only if using with ultra sonic
+    robot.straight(150)
 
-    while touch_sensor.pressed() == False:
-        robot.straight(5)
+    #while touch_sensor.pressed() == False:
+        #robot.straight(5)
     
-    lift_motor.run_target(5, 15, Stop.HOLD, True)
-
+    lift_motor.run_target(200, 55, Stop.HOLD, True)
+    robot.straight(-200)
     check_pick_up()
-
-def checkLight():
-    lightLVL = color_sensor.rgb()
-    print(lightLVL)
-    time.sleep(1)
-    return lightLVL
 
 def main(): # Main method
-    checkLight()
+    find_item()
+    time.sleep(5)
+    lift_motor.run_target(200, -10)
     return 0
 
 if __name__ == '__main__':
